d76ce4d5b755c76dad669247ab03bcfd264a87ee
hmczap <114511170+hmczap@users.noreply.github.com>
2024-01-18 02:33:48 -0500

    Add media query for mobile styles, using flex-direction: column

---
+++ resources/js/main.js
@@ -0,0 +1,9 @@
+// take the data from the data.json file stored locally, 
+// and use javascript to insert it into the page
+
+let main = document.getElementById("main");
+
+// get handle to the results list
+//read in results from data.json (using the parse() method)
+// for each list element in the results list, insert the score result and icon
+//  into the innerhtml / innertext of the list element

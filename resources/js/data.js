f5c67cd5c3289c60020167e242494f293ebf2ca9
hmczap <114511170+hmczap@users.noreply.github.com>
2024-03-06 00:27:42 -0500

    Added solution screenshots as images, modified main.js to dynamically add test results to the document

---
+++ resources/js/data.js
@@ -0,0 +1,22 @@
+var object1 = [
+  {
+    "category": "Reaction",
+    "score": 80,
+    "icon": "./resources/images/icon-reaction.svg"
+  },
+  {
+    "category": "Memory",
+    "score": 92,
+    "icon": "./resources/images/icon-memory.svg"
+  },
+  {
+    "category": "Verbal",
+    "score": 61,
+    "icon": "./resources/images/icon-verbal.svg"
+  },
+  {
+    "category": "Visual",
+    "score": 72,
+    "icon": "./resources/images/icon-visual.svg"
+  }
+]
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,9 +1,35 @@
-// take the data from the data.json file stored locally, 
-// and use javascript to insert it into the page
+window.addEventListener("load", () => {
+    let totalResult = document.getElementById('results-list');
+    for (let i = 0; i < object1.length; i++){
+        // create and append list element
+        let listResult = document.createElement("li");
+        listResult.className = object1[i].category.toLowerCase();
 
-let main = document.getElementById("main");
+        // create html elements for each result + icon
+        let icon = document.createElement("div");
+        icon.className = "results-icon";
+        icon.innerHTML = `<img src=${object1[i].icon} alt="">`;
+
+        let title = document.createElement("h5");
+        title.innerText = object1[i].category;
+
+        let score = document.createElement("p");
+        score.innerHTML = `<strong class="user-result">${object1[i].score}</strong>`;
+        score.innerHTML += '/100';
+
+        // append result to list element
+        listResult.appendChild(icon);
+        listResult.appendChild(title);
+        listResult.appendChild(score);
+
+        // append list element to list
+        totalResult.appendChild(listResult)
+    }
+})
+
 
 // get handle to the results list
 //read in results from data.json (using the parse() method)
 // for each list element in the results list, insert the score result and icon
 //  into the innerhtml / innertext of the list element
+
